1608d670d13f17f5a3eb09cadd8876b4323e428d
Hyrum Sorensen <hyrum@lenderdock.com>
2025-04-27 12:06:54 -0600

    testing icon stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,14 @@ export default function RootLayout({
   return (
     <ConvexAuthNextjsServerProvider>
       <html lang="en">
+      <head>
+          {/* PWA Manifest and Icon setup */}
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon-192x192.png" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        </head>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
